aa445d2003320d9a6f56b40a61bba3a81160cba9
Anna Akulova <redschoolgirl@gmail.com>
2026-08-23 17:00:48 +0300

    Add nested Lurea food photo guide

--- scripts/inject-favicon-fallback.rb
+++ scripts/inject-favicon-fallback.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 site_root = File.expand_path(ARGV.fetch(0))
 root_index = File.join(site_root, 'index.html')
 
@@ -19,19 +21,33 @@ Dir.glob(File.join(site_root, '**', 'index.html')).sort.each do |path|
   end
   next if has_own_favicon
 
-  local_favicon = favicon_candidates.keys.find do |filename|
-    File.file?(File.join(File.dirname(path), filename))
+  page_dir = File.dirname(path)
+  source_dir = page_dir
+  favicon_filename = nil
+
+  loop do
+    favicon_filename = favicon_candidates.keys.find do |filename|
+      File.file?(File.join(source_dir, filename))
+    end
+    break if favicon_filename || source_dir == site_root
+
+    parent_dir = File.dirname(source_dir)
+    break unless parent_dir.start_with?(site_root)
+
+    source_dir = parent_dir
   end
 
-  favicon_links = if local_favicon
-    type = favicon_candidates.fetch(local_favicon)
-    %(<link rel="icon" type="#{type}" href="#{local_favicon}">)
-  else
-    <<~HTML.chomp
-      <link rel="icon" type="image/png" sizes="64x64" href="/trips/favicon.png">
-      <link rel="apple-touch-icon" sizes="180x180" href="/trips/apple-touch-icon.png">
-    HTML
-  end
+  abort "Не найден favicon для #{path}" unless favicon_filename
+
+  favicon_path = File.join(source_dir, favicon_filename)
+  favicon_href = Pathname.new(favicon_path).relative_path_from(Pathname.new(page_dir)).to_s
+  type = favicon_candidates.fetch(favicon_filename)
+  favicon_links = %(<link rel="icon" type="#{type}" href="#{favicon_href}">)
+
+  touch_path = File.join(source_dir, 'apple-touch-icon.png')
+  touch_path = favicon_path unless File.file?(touch_path)
+  touch_href = Pathname.new(touch_path).relative_path_from(Pathname.new(page_dir)).to_s
+  favicon_links += %(\n<link rel="apple-touch-icon" href="#{touch_href}">)
 
   updated = html.sub(/<\/head>/i, "#{favicon_links}\n</head>")
   abort "Не найден </head> в #{path}" if updated == html
